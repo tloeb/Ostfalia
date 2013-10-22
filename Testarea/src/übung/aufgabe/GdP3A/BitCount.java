dbdce417273c77efc00a19e5d8eed219229ce28e
Generation0 <tobias_loeb@web.de>
2013-10-22 21:59:05 +0200

    without array, now with a String

--- Testarea/src/übung/aufgabe/GdP3A/BitCount.java
+++ Testarea/src/übung/aufgabe/GdP3A/BitCount.java
@@ -9,11 +9,11 @@ public class BitCount {
 		this.anyint = i;
 	}
 
-	public int[] getBits() {
+	public String getBits() {
 
 		// Herausfinden wie viele Stellen benötigt werden
 		int count = 1;
-		
+
 		int tempresult = 1;
 		int rest = anyint;
 
@@ -21,9 +21,9 @@ public class BitCount {
 			tempresult = (int) Math.pow(2, (double) count);
 			count++;
 		} while (anyint > tempresult);
-	
+
 		// Die Dualzahl wird in ein Array geschrieben
-		int[] dual = new int[count];
+		String dual = "";
 
 		// Einsen finden
 		for (int i = count; i > 0; i--) {
@@ -31,21 +31,25 @@ public class BitCount {
 			if (val <= rest) {
 				rest = rest - val;
 				bitanzahl++;
-				dual[i-1] = 1; // Einsen in das Array schreiben, -1 da der Index von 0-3 geht
-			} 
+				dual += '1'; // Einsen in das Array schreiben, -1 da der Index
+								// von 0-3 geht
+			} else
+				dual += '0';
+		
 		}
 
 		return dual;
 	}
 	
-	//Hilfsmethode zur Ausgabe der Dualzahl als String
-	public String toString(int[] in) {
-		String out = "";
-		for (int i = in.length-1; i >= 0; i--) {
-			out += in[i];
-		}
+	public int toInt(String s){
+		int out = 00;
+		
+		
+		
 		return out;
+		
 	}
+
 
 	public int getBitanzahl() {
 		return this.bitanzahl;
